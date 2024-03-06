eec2d1d9a33e4aeaa3993ad3b5badb6314c73277
Natalia Krzus <nkrzus95@gmail.com>
2024-03-06 23:24:46 +0100

    change type of component on file ingredient and ingrtedients

--- app/src/cookbook/ingredient.jsx
+++ app/src/cookbook/ingredient.jsx
@@ -1,9 +1,6 @@
 import React from "react";
-
-export default class Ingredient extends React.Component {
-    render() {
-        return <li>
-            {this.props.item}
-        </li>
-    }
+export default function Ingredient (props){
+    return <li>
+        {props.item}
+    </li>
 }
--- app/src/cookbook/ingredients.jsx
+++ app/src/cookbook/ingredients.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import Ingredient from "./ingredient";
-export default class Ingredients extends React.Component {
 
-    render() {
-        return <div>
-            <h3>Składniki:</h3>
-            {this.showIngredients()}
-        </div>
-    }
-
-
-    showIngredients() {
-        return <ul>
-            {this.props.elements.map(el => {
-                return <Ingredient item={el} />;
-            })}
-        </ul>;
-    }
+export default function Ingredients(props){
+const showIngredients=()=>{
+    return <ul>
+        {props.elements.map(el=>{return <Ingredient item={el}/>})}
+    </ul>
+}
+return <div>
+    <h3>Składniki:</h3>
+    {showIngredients()}
+</div>
 }
